29561dd48f254cdf05faeb5e2479a99fb7782376
Ahmed5Emad <ahmed2077emad@gmail.com>
2025-10-16 00:45:49 +0300

    fix: Configure basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/itlegend-test",
+  output: "export",
 };
 
 export default nextConfig;
