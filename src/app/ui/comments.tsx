76611fd4f646b4665ec14c7f342585e7a8d5761b
Ahmed5Emad <ahmed2077emad@gmail.com>
2025-10-16 00:33:46 +0300

    feat: Update comments with new avatars

--- src/app/ui/comments.tsx
+++ src/app/ui/comments.tsx
@@ -7,21 +7,21 @@ export default function Comments() {
   const comments = [
     {
       name: "Student Name Gose Here",
-      imgurl: "https://placehold.co/100x100/EFEFEF/333?text=SN&font=sans-serif", // Placeholder avatar
+      imgurl: "/images/avatar1.webp", // Placeholder avatar
       date: "Oct 10,2021",
       comment:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem facilis tempore reiciendis laboriosam voluptatum dicta expedita aliquam nesciunt vitae eius!",
     },
     {
       name: "Student Name Gose Here",
-      imgurl: "https://placehold.co/100x100/EFEFEF/333?text=SN&font=sans-serif", // Placeholder avatar
+      imgurl: "/images/avatar2.webp", // Placeholder avatar
       date: "Oct 15,2021",
       comment:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem facilis tempore reiciendis laboriosam voluptatum dicta expedita aliquam nesciunt vitae eius!",
     },
     {
       name: "Student Name Gose Here",
-      imgurl: "https://placehold.co/100x100/EFEFEF/333?text=SN&font=sans-serif", // Placeholder avatar
+      imgurl: "/images/avatar3.webp", // Placeholder avatar
       date: "Oct 19,2021",
       comment:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem facilis tempore reiciendis laboriosam voluptatum dicta expedita aliquam nesciunt vitae eius!",
